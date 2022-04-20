71686dcbaf22030b02bf7011ccaa2f11f4e2dd9c
Iris Chien <irischien@gmail.com>
2022-04-20 14:15:22 -0400

    accesslog

--- database.js
+++ database.js
@@ -8,7 +8,7 @@ const db = new Database('log.db');
 
 // Is the database initialized or do we need to initialize it?
 const stmt = db.prepare(`
-    SELECT name FROM sqlite_master WHERE type='table' and name='accesslogs';`
+    SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`
     );
 // Define row using `get()` from better-sqlite3
 let row = stmt.get();
